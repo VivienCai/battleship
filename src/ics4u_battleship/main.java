5f3b888af676942ad01ba414410e0486eaaf45ae
ushering123 <jiaan124@gmail.com>
2021-11-24 10:26:43 -0500

    hello

--- src/ics4u_battleship/main.java
+++ src/ics4u_battleship/main.java
@@ -1,7 +1,10 @@
 package ics4u_battleship;
 
 public class main {
-    public static void main(String[] args) {
-        System.out.println("hellow rold");
-    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
