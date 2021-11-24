b12a201790707b0f3f24a4413fe57b1879efd631
VivienCai <74844967+VivienCai@users.noreply.github.com>
2021-11-24 10:27:13 -0500

    k

--- src/ics4u_battleship/main.java
+++ src/ics4u_battleship/main.java
@@ -2,9 +2,9 @@ package ics4u_battleship;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // he;
+    }
 
 }
