ad8c2f068c4a72a220542ce7ee6240f480141bbe
VivienCai <74844967+VivienCai@users.noreply.github.com>
2021-12-10 09:16:07 -0500

    gettiner and setting

--- src/AI.java
+++ src/AI.java
@@ -1,19 +1,28 @@
 public class AI {
     boolean isHunting;
+    Coordinate test;
 
     // placing algorithm
-    public void place() {
+    public static void place() {
         // for every ship
         for (int shipSize = 2; shipSize <= 5; shipSize++) {
-            int x = (int) Math.random() * (10 - shipSize);
-            int y = (int) Math.random() * (10 - shipSize);
+            boolean orientationV = (int) ((Math.random() * 2) + 1) == 1 ? true : false;
+            
+            if (orientationV) {
+                
+            }
+
+            // int x = (int) (Math.random() * (10 - 5));
+            // int y = (int) (Math.random() * (10 - 5));
+            // System.out.println(x);
+            // System.out.println(y);
+            // System.out.println((int) ((Math.random() * 2) + 1));
         }
 
     }
 
     public static void main(String[] args) {
-        System.out.println(x);
-        System.out.println(y);
+        place();
     }
 
 }
--- src/Coordinate.java
+++ src/Coordinate.java
@@ -6,4 +6,20 @@ public class Coordinate {
         x_coor = x;
         y_coor = y;
     }
+
+    public int getX() {
+        return x_coor;
+    }
+
+    public int getY() {
+        return y_coor;
+    }
+
+    public void setX(int newX) {
+        x_coor = newX;
+    }
+
+    public void sety(int newY) {
+        y_coor = newY;
+    }
 }
