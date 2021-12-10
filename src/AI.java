ce858ae7fedb3111c0df50068f7131045eac5850
VivienCai <74844967+VivienCai@users.noreply.github.com>
2021-12-10 09:02:05 -0500

    coor and placing

--- src/AI.java
+++ src/AI.java
@@ -1,3 +1,19 @@
 public class AI {
-    
+    boolean isHunting;
+
+    // placing algorithm
+    public void place() {
+        // for every ship
+        for (int shipSize = 2; shipSize <= 5; shipSize++) {
+            int x = (int) Math.random() * (10 - shipSize);
+            int y = (int) Math.random() * (10 - shipSize);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(x);
+        System.out.println(y);
+    }
+
 }
--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,3 +1,9 @@
 public class Coordinate {
-    
+    private int x_coor;
+    private int y_coor;
+
+    public Coordinate(int x, int y) {
+        x_coor = x;
+        y_coor = y;
+    }
 }
