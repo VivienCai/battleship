d11a0cca70f076a864e3b3c4a934133ec6c19b4d
ushering123 <jiaan124@gmail.com>
2021-12-14 09:48:33 -0500

    Update hunt.java

--- src/hunt.java
+++ src/hunt.java
@@ -2,44 +2,55 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class hunt extends AI{
-    static int sum[][] = new int[11][11];
-    static Scanner sc = new Scanner(System.in);
-    static int max = 0;
-    static ArrayList<Coordinate> possibleHits = new ArrayList<Coordinate>();
-    static int size;
+    private static int sum[][] = new int[11][11];
+   private  static Scanner sc = new Scanner(System.in);
+   private static int max = 0;
+   private static ArrayList<Coordinate> possibleHits = new ArrayList<Coordinate>();
+   private static int size;
   //  static Coordinate board[][];
-    static Coordinate hitPoint;
-    static boolean shipExists[]=new boolean[5];
-	static int rangeL=size-1;
-	static int rangeR=size-1;
-	static int rangeU=size-1;
-	static int rangeD=size-1;
+   private static Coordinate hitPoint;
+   private static boolean shipExists[]=new boolean[5];
+   private	static int rangeL=size-1;
+   private	static int rangeR=size-1;
+   private	static int rangeU=size-1;
+   private	static int rangeD=size-1;
+   private   static char huntBoard[][]=new char[11][11];
+   static int tempsum=0;
+   
+    	public static void main(String args[]) {
+		Coordinate hitTest=new Coordinate(5,5);
+		initBoard();
+		initSum();
+		hunt1(hitTest, huntBoard);
+	}
+
+	
     
-    
-    
-	public static void hunt1(Coordinate hitPoint1, Coordinate board [][]) {
+	public static void hunt1(Coordinate hitPoint1, char board [][]) {
 		hitPoint=hitPoint1;
 		// O - not hit or occupied
 	    // X - occupied by a ship, hit
 	    // M - not occupied by a ship, hit
 	    // S - occupied by a ship, not hit
 	//	Coordinate board[][]=board1.clone();
+		exist();  //sets all ships to exist
 		for (int i=0;i<5;i++) {
 			if (shipExists[i]) {
 				if (i==4) {
 					size=3; //add some sum stuff
+        			System.out.print("hi1");
+
+					sumX(size);
+					sumY(size);
 				}
 				else {
 					size=i+2;
-				}
-				
-				
-				
+					sumX(size);
+					sumY(size);
+				}	
 			}
 		}
-		
-		
-		
+	
 	}
 	
 
@@ -50,22 +61,24 @@ public class hunt extends AI{
     	    	
     	
     	for(int j=hitPoint.getX()-rangeL;j<(hitPoint.getX()+rangeR)-size;j++) {  //runs for the number of open squares 
-    		if(size==3) {
+    		if(shipSize==3) {
     			for (int g=j;g<j;g++) {
         			sum[g][hitPoint.getY()] +=2;  //i have no clue what this does it was copied from sarinas code and its supposed to sum stuff
         		}
+    			printArray();
+    			System.out.print("hi2");
+
     		}
     		else{
     			for (int g=j;g<j;g++) {
         			sum[g][hitPoint.getY()] +=1;
-        		}
-    		}
-    		
-    	}
-    	
-    	
+        			printArray();
+        			System.out.print("hi");
 
-    	    
+        		}
+    			
+    		}	
+    	}	    
     }
 
     public static void sumY(int shipSize) {
@@ -74,18 +87,29 @@ public class hunt extends AI{
     	
     	       
     	for(int j=hitPoint.getY()-rangeU;j<(hitPoint.getY()+rangeD)-size;j++) {  //runs for the number of open squares 
+			System.out.println("joe1");
+
     		if(size==3) {
     			for (int g=j;g<j;g++) {
         			sum[hitPoint.getX()][g] +=2;  //i have no clue what this does it was copied from sarinas code and its supposed to sum stuff
-        		}
+        			System.out.println("joe2");
+        			tempsum++;
+    			}
     		}
     		else{
     			for (int g=j;g<j;g++) {
+    				System.out.println("joe3");
         			sum[hitPoint.getX()][g] +=1;
-        		}
+        			tempsum++;
+
+    			}
     		}
     		
     	}
+    	printArray();
+    	System.out.println(hitPoint.getX());
+    	System.out.println(sum[5][5]);
+    	System.out.println(tempsum);
     }
 	
 	
@@ -194,6 +218,22 @@ public class hunt extends AI{
         } catch (Exception e) {
             return false;// else, return false
         }
+    }
+    
+    public static void initBoard() {
+    	for (int i=0;i<11;i++) {
+    		for (int j=0;j<11;j++) {
+    			huntBoard[i][j]='O';
+    		}
+    	}
+    }
+    
+    public static void initSum() {
+    	for (int i=0;i<11;i++) {
+    		for (int j=0;j<11;j++) {
+    			sum[i][j]=0;
+    		}
+    	}
     }
 
 }
