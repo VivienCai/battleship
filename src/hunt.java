af659803b33e31a2f228683a4c8debc844397620
ushering123 <jiaan124@gmail.com>
2021-12-15 19:53:46 -0500

    hi

--- src/hunt.java
+++ src/hunt.java
@@ -2,27 +2,27 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class hunt extends AI{
+	  private  static Scanner sc = new Scanner(System.in);
+
    private static int sum[][] = new int[11][11];
-   private  static Scanner sc = new Scanner(System.in);
-   private static int max = 0;
+   private static int max,timesMissed, counter = 0;
    private static ArrayList<Coordinate> possibleHits = new ArrayList<Coordinate>();
+   private static ArrayList<Coordinate> hitPoints = new ArrayList<Coordinate>();
+
    private static int size;
   //  static Coordinate board[][];
    private static Coordinate hitPoint;
    private static boolean shipExists[]=new boolean[5];
-   private	static int rangeL=size-1;
-   private	static int rangeR=size-1;
-   private	static int rangeU=size-1;
-   private	static int rangeD=size-1;
+   private	static int rangeL, rangeR, rangeU, rangeD=size-1;
    private  static char board[][]=new char[11][11];
-   private static int timesHit=0;
-   private static int timesMissed=0;
    private static boolean didHit;
    private static boolean sunk=false;
+   static int timesHit=1;
     	public static void main(String args[]) {  //testing stuff
-		Coordinate hitTest=new Coordinate(4,4);                              //CHANGE THIS TO FIND WHICH POINT YOU WANT TO HIT
+		Coordinate hitTest=new Coordinate(5,5);                              //CHANGE THIS TO FIND WHICH POINT YOU WANT TO HIT
 		initBoard();
-		board[5][4]='y';                                                       //CHANGE THIS TO SIMULATE ALREADY HIT OR MISSED POINTS
+		board[4][7]='S';                                                       //CHANGE THIS TO SIMULATE ALREADY HIT OR MISSED POINTS
+		board[1][3]='S';                                                       //CHANGE THIS TO SIMULATE ALREADY HIT OR MISSED POINTS
 
 
 		printBoard();
@@ -33,7 +33,11 @@ public class hunt extends AI{
 	
     //actual hunt code
 	public static void hunt1(Coordinate hitPoint1, char board [][]) {
+		hitPoints.add(counter, hitPoint1);
+		
 		initSum();
+		  max=0;
+		  possibleHits.clear();
 		hitPoint=hitPoint1;
 		// O - not hit or occupied
 	    // X - occupied by a ship, hit
@@ -56,7 +60,6 @@ public class hunt extends AI{
 			}
 		}
 		sum[hitPoint.getY()][hitPoint.getX()]=0;   //makes the value of hit point=0 so ai wont hit there again
-		
 		for (int i = 1; i <= 10; i++) {     //find the max number of the array
 			for (int j = 1; j <= 10; j++) {
 				max = Math.max(max, sum[i][j]);
@@ -72,28 +75,127 @@ public class hunt extends AI{
       int randIndex = (int) (Math.random() * possibleHits.size());
       Coordinate hit = possibleHits.get(randIndex);
       System.out.printf("The AI hit coordinate %c%d\n", hit.columnIndex(hit.getY()), hit.getX()); //displaying hit place
-      
+      printArray();
+	printBoard();
       System.out.println("Did your ship get hit? Please enter yes or no");
       String temp=sc.nextLine();
-	  if (temp.equals("yes")) {
+		
+		System.out.println(hit.getY()+"h"+hit.getX());
+	  if (temp.equals("yes")) { //stores either hit or miss
 		  didHit=true;
+		  board[hit.getY()][hit.getX()]='X';
+		  counter++;
+		  hitPoints.add(counter, hitPoint1); 
+		  hunt2(hitPoint,board);
 	  }
 	  else {
 		  didHit=false;
+		  board[hit.getY()][hit.getX()]='S';
+		  hunt1(hitPoint,board);
 	  }
 	  
-	  //start of 2nd or more time hitting
-//	  while (!sunk) {  //will keep on hitting until ship is sunk  
-//	  }
+	  //Below code should only be reached when ship is sunk
+	  System.out.println("I sunk your "+timesHit+" unit long ship");
+	  
+	  
+
+	  
+	 
 	  
 }
-	
-	
+
+	public static void hunt2(Coordinate hitPoint1, char board [][]) {
+		initSum();
+		possibleHits.clear();
+		  if(hitPoints.get(0).getX()==hitPoints.get(1).getX()) {  //if ship horizontal
+			  for (int i=0;i<5;i++) {
+					if (shipExists[i]) {
+						if (i==4) {
+							size=3;              //sums the possible ship locations
+							sumX(size-1);
+						}
+						else {
+							size=i+2;
+							sumX(size-1);
+						}	
+					}
+				}
+			  printArray();
+			  System.out.println("^^ right after sum");
+		  }
+		  
+		  else if (hitPoints.get(0).getY()==hitPoints.get(1).getY());{
+			  
+			  System.out.println(hitPoints.get(0).getY()+"Y2"+hitPoints.get(1).getY() );
+			  for (int i=0;i<5;i++) {
+					if (shipExists[i]) {
+						if (i==4) {
+							size=3;              //sums the possible ship locations
+							sumY(size-1);
+						}
+						else {
+							size=i+2;
+							sumY(size-1);
+						}	
+					}
+				}
+			  
+			  
+		  }
+		  printArray();
+		 for (int i=0;i<hitPoints.size();i++) {
+			 sum[hitPoints.get(i).getY()][hitPoints.get(i).getX()]=0;
+		 }                                          //makes the value of hit point=0 so ai wont hit there again
+		  printArray();
+
+			for (int i = 1; i <= 10; i++) {     //find the max number of the array
+				for (int j = 1; j <= 10; j++) {
+					max = Math.max(max, sum[i][j]);
+					}
+				}
+			for (int i = 1; i <= 10; i++) {
+				for (int j = 1; j <= 10; j++) {
+					if (sum[i][j] == max) {
+						possibleHits.add(new Coordinate(i, j));	  
+					}
+				}
+			}	
+		  int randIndex = (int) (Math.random() * possibleHits.size());
+		  Coordinate hit = possibleHits.get(randIndex);
+	      System.out.println("The coordinate hit is "+hit.columnIndex(hit.getY()) +" "+hit.getX());
+		    //  System.out.printf("The AI hit coordinate %c%d\n", hit.columnIndex(hit.getY()), hit.getX()); //displaying hit place
+	      printArray();
+	      System.out.println("Did your ship get hit? Please enter yes or no");
+	      String temp=sc.nextLine();
+	      if(temp.equals("yes")) {
+	    	  timesHit++;
+		      System.out.println("Did your ship get sunk? Please enter yes or no");
+		      temp=sc.nextLine();
+		      if (temp.equals("yes")) {
+		    	  System.out.println("yay");
+		    	                               //do smtg to remove ship from future searches end program
+		      }
+		      else {
+				  board[hit.getY()][hit.getX()]='X';
+
+		    	  hunt2(hitPoint, board);                           //call program again
+		      }
+	      }
+	      
+	      else {
+			  board[hit.getY()][hit.getX()]='S';
+
+	      }
+	      
+		  
+		  
+	  
+	}
 	
 	
 
     public static void sumX(int shipSize) {
-    	nearby(false, false);                  //initialise range values. find nearby occupied spots
+    	nearby(false, false);                  //initialize range values. find nearby occupied spots
     	nearby(false,true);
     	
     //	 System.out.println(rangeL+" "+rangeR);   	
@@ -133,6 +235,8 @@ public class hunt extends AI{
 		char boardValue;
 		int boardAscii;
 		int targetAscii=79;
+		int targetAscii2=88;
+
 		if (isV==false&&positive==true) {                                //any obstructions to the right
 			for (int i=1;i<size;i++) {
 				if (hitPoint.getX()+i>10) { //makes sure code doesnt crash
@@ -141,7 +245,7 @@ public class hunt extends AI{
 				}
 				boardValue=board[hitPoint.getY()][hitPoint.getX()+i];
 				boardAscii=boardValue;
-				if (targetAscii==boardAscii) {  //if not open  	                                                 //ADD EDGE DETECTION AFOISHEFIUSHFIKJSEF
+				if (targetAscii==boardAscii||targetAscii2==boardAscii) {  //if not open  	                                                 //ADD EDGE DETECTION AFOISHEFIUSHFIKJSEF
 					rangeR=size-1;
 				}
 				else {
@@ -159,7 +263,7 @@ public class hunt extends AI{
 				}
 				boardValue=board[hitPoint.getY()][hitPoint.getX()-i];
 				boardAscii=boardValue;
-				if (targetAscii==boardAscii) {  //if not open  	                                                 //ADD EDGE DETECTION AFOISHEFIUSHFIKJSEF
+				if (targetAscii==boardAscii||targetAscii2==boardAscii) {  //if not open  	                                                 //ADD EDGE DETECTION AFOISHEFIUSHFIKJSEF
 					rangeL=size-1;
 				}
 				else {
@@ -178,7 +282,7 @@ public class hunt extends AI{
 				}
 				boardValue=board[hitPoint.getY()-i][hitPoint.getX()];
 				boardAscii=boardValue;
-				if (targetAscii==boardAscii) {  //if not open  	                                                 //ADD EDGE DETECTION AFOISHEFIUSHFIKJSEF
+				if (targetAscii==boardAscii||targetAscii2==boardAscii) {  //if not open  	                                                 //ADD EDGE DETECTION AFOISHEFIUSHFIKJSEF
 					rangeU=size-1;
 
 				}
@@ -198,7 +302,7 @@ public class hunt extends AI{
 				}
 				boardValue=board[hitPoint.getY()+i][hitPoint.getX()];
 				boardAscii=boardValue;
-				if (targetAscii==boardAscii) {  //if not open  	                                                 //ADD EDGE DETECTION AFOISHEFIUSHFIKJSEF
+				if (targetAscii==boardAscii||targetAscii2==boardAscii) {  //if not open  	                                                 //ADD EDGE DETECTION AFOISHEFIUSHFIKJSEF
 					rangeD=size-1;
 
 				}
