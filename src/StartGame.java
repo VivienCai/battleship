3ff50a57b2b24b44b341bb9ad6b361fa5c900784
VivienCai <74844967+VivienCai@users.noreply.github.com>
2022-01-07 09:26:29 -0500

    womannnnn

--- src/StartGame.java
+++ src/StartGame.java
@@ -8,11 +8,8 @@
 
 public class StartGame {
 	public static void main(String[] args) throws Exception {
-
 		Main.initArrays();
 		Game.initHitPoint();
-		// Placing.place(Main.AIPlacementBoard);							//moved this somewhere else cause it kept on bugging my file handling
-	//	Game.printPlacementArray(Main.AIPlacementBoard);
 		GUI.setUpWindow();
 	}
 }
